873bc6a0b97230659cdfad3c05f41d2ad66f35e2
Santiago Daniele <santiago_daniele@hotmail.com>
2022-08-13 11:59:00 -0300

    hamburger.js

---
+++ public/js/hamburguer.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    document.getElementById("menu-bar").addEventListener('click', modificarMenu);
+
+    var discriminador = true;
+
+    window.addEventListener('resize', ocultarMenu);
+
+    function modificarMenu() {
+        if (discriminador) {
+            document.querySelector('nav').style.left = '-0px';
+            discriminador = false
+        }
+        else {
+            document.querySelector('nav').style.left = '-140px';
+            discriminador = true;
+        }
+
+    }
+    function ocultarMenu() {
+        if (window.innerWidth < 768) {
+            document.querySelector('nav').style.left = '-140px';
+            discriminador = true;
+        }
+
+    }
+
+
+}

06b35181b2d5d9109fb9a2e74df9a3ce4d13fd5c
agufelice <agustinfelice@hotmail.com>
2022-08-26 16:39:16 -0300

    Editar Perfil y cambio de Rol

--- src/controllers/api/productsAPIController.js
+++ src/controllers/api/productsAPIController.js
@@ -17,164 +17,35 @@ const productsAPIController = {
         db.Product.findAll({
             include: ['category']
         })
-        .then(products => {
-            let respuesta = {
-                meta: {
-                    status : 200,
-                    total: products.length,
-                    url: 'api/products'
-                },
-                data: products
-            }
+            .then(products => {
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        total: products.length,
+                        url: 'api/products'
+                    },
+                    data: products
+                }
                 res.json(respuesta);
             })
-    },    
+    },
     'detail': (req, res) => {
-        db.Product.findByPk(req.params.id,
-            {
-                include : ['category']
-            })
+        db.Product.findOne({
+            order: [['id', 'DESC']],
+            include: ['category', "variety"],
+        })
             .then(product => {
                 let respuesta = {
                     meta: {
                         status: 200,
                         total: product.length,
-                        url: '/api/product/:id'
+                        url: '/api/products/detail'
                     },
                     data: product
                 }
                 res.json(respuesta);
             });
     },
-    // 'recomended': (req, res) => {
-    //     db.Movie.findAll({
-    //         include: ['genre'],
-    //         where: {
-    //             rating: {[db.Sequelize.Op.gte] : req.params.rating}
-    //         },
-    //         order: [
-    //             ['rating', 'DESC']
-    //         ]
-    //     })
-    //     .then(movies => {
-    //         let respuesta = {
-    //             meta: {
-    //                 status : 200,
-    //                 total: movies.length,
-    //                 url: 'api/movies/recomended/:rating'
-    //             },
-    //             data: movies
-    //         }
-    //             res.json(respuesta);
-    //     })
-    //     .catch(error => console.log(error))
-    // },
-    // create: (req,res) => {
-    //     Movies
-    //     .create(
-    //         {
-    //             title: req.body.title,
-    //             rating: req.body.rating,
-    //             awards: req.body.awards,
-    //             release_date: req.body.release_date,
-    //             length: req.body.length,
-    //             genre_id: req.body.genre_id
-    //         }
-    //     )
-    //     .then(confirm => {
-    //         let respuesta;
-    //         if(confirm){
-    //             respuesta ={
-    //                 meta: {
-    //                     status: 200,
-    //                     total: confirm.length,
-    //                     url: 'api/movies/create'
-    //                 },
-    //                 data:confirm
-    //             }
-    //         }else{
-    //             respuesta ={
-    //                 meta: {
-    //                     status: 200,
-    //                     total: confirm.length,
-    //                     url: 'api/movies/create'
-    //                 },
-    //                 data:confirm
-    //             }
-    //         }
-    //         res.json(respuesta);
-    //     })    
-    //     .catch(error => res.send(error))
-    // },
-    // update: (req,res) => {
-    //     let movieId = req.params.id;
-    //     Movies.update(
-    //         {
-    //             title: req.body.title,
-    //             rating: req.body.rating,
-    //             awards: req.body.awards,
-    //             release_date: req.body.release_date,
-    //             length: req.body.length,
-    //             genre_id: req.body.genre_id
-    //         },
-    //         {
-    //             where: {id: movieId}
-    //     })
-    //     .then(confirm => {
-    //         let respuesta;
-    //         if(confirm){
-    //             respuesta ={
-    //                 meta: {
-    //                     status: 200,
-    //                     total: confirm.length,
-    //                     url: 'api/movies/update/:id'
-    //                 },
-    //                 data:confirm
-    //             }
-    //         }else{
-    //             respuesta ={
-    //                 meta: {
-    //                     status: 204,
-    //                     total: confirm.length,
-    //                     url: 'api/movies/update/:id'
-    //                 },
-    //                 data:confirm
-    //             }
-    //         }
-    //         res.json(respuesta);
-    //     })    
-    //     .catch(error => res.send(error))
-    // },
-    // destroy: (req,res) => {
-    //     let movieId = req.params.id;
-    //     Movies
-    //     .destroy({where: {id: movieId}, force: true}) // force: true es para asegurar que se ejecute la acción
-    //     .then(confirm => {
-    //         let respuesta;
-    //         if(confirm){
-    //             respuesta ={
-    //                 meta: {
-    //                     status: 200,
-    //                     total: confirm.length,
-    //                     url: 'api/movies/destroy/:id'
-    //                 },
-    //                 data:confirm
-    //             }
-    //         }else{
-    //             respuesta ={
-    //                 meta: {
-    //                     status: 204,
-    //                     total: confirm.length,
-    //                     url: 'api/movies/destroy/:id'
-    //                 },
-    //                 data:confirm
-    //             }
-    //         }
-    //         res.json(respuesta);
-    //     })    
-    //     .catch(error => res.send(error))
-    // }
-    
 }
 
 module.exports = productsAPIController;
